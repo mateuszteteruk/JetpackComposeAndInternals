1ac7efb6a3be48d45ae70eba0c1f9e35dfd48e85
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2024-01-16 08:31:18 +0100

    feat: Implement test for Exercise 4

--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/LazySpeakersScreen.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/LazySpeakersScreen.kt
@@ -3,6 +3,8 @@ package dev.jorgecastillo.compose.app.ui.composables
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -23,7 +25,33 @@ import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
 
 @Composable
 fun LazySpeakersScreen(speakers: List<Speaker>) {
-    
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Speakers") })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    painter = rememberVectorPainter(image = Icons.Default.Add),
+                    contentDescription = stringResource(id = R.string.content_desc_fab_add_speaker),
+                )
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(paddingValues)
+                .testTag("SpeakersList"),
+        ) {
+            items(
+                items = speakers,
+                key = { it.id },
+            ) { speaker ->
+                SpeakerCard(speaker = speaker)
+            }
+        }
+    }
+
 }
 
 @Composable
